f5c3927904faca6812ab8aee444fa19344c713d2
Sergey Sharpov <sharpov.sergey@theevenstar.net>
2023-08-18 18:28:47 +0300

    hotfixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import {Canvas} from '@react-three/fiber'
-import {HomePage} from "./pages";
+import { Suspense, useEffect } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { HomePage } from "./pages";
+import Loader, { useLoaderStore } from './components/Loader';
+
+const Fallback = () => {
+    const setLoading = useLoaderStore((state) => state.setLoading);
+
+    useEffect(() => {
+        setLoading(true);
+
+        return () => {
+            setLoading(false);
+        }
+    }, [setLoading])
+
+    return null;
+}
 
 const App = () => {
     return (
         <div className="w-screen h-screen overflow-hidden fixed inset-0">
-            <Canvas>
-                <color attach="background" args={["#213547"]}/>
-                <fog attach="fog" args={["#213547", 1, 1000]}/>
-                <HomePage/>
-            </Canvas>
+            <Suspense fallback={<Fallback />}>
+                <Canvas>
+                    <HomePage/>
+                </Canvas>
+            </Suspense>
+            <Loader />
         </div>
     );
 }
