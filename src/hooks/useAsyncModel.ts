a47e2060db8bcc7d0c687ce10ed0fe808cd46663
BorisKutsenko <boris.k@theevenstar.net>
2023-09-04 13:06:59 +0300

    feature - add new model and DRACOLoader

--- src/hooks/useAsyncModel.ts
+++ src/hooks/useAsyncModel.ts
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {Group} from "three";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 
 export const useAsyncModel = (modelPath: string) => {
     const [model, setModel] = useState<Group | null>(null);
@@ -8,6 +9,10 @@ export const useAsyncModel = (modelPath: string) => {
     useEffect(() => {
         const loadAsync = async () => {
             const loader = new GLTFLoader();
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+
+            loader.setDRACOLoader(dracoLoader)
 
             const loadedModel = await loader.loadAsync(modelPath);
             setModel(loadedModel.scene);
